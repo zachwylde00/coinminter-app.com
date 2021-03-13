f77f9f890c6f68af53faba594898d8dc622ec554
John J. Pruett II <43480274+zachwylde00@users.noreply.github.com>
2021-03-13 01:59:15 -0600

    Create Index.js

---
+++ Scripts/Styles/Index.js
@@ -0,0 +1,1 @@
+.theme-container .hero img border-radius 50% max-height 200px !important .sidebar-heading display none .navbar .site-name display none .theme-default-content code color #2c3e50 !important .fab padding .2em pre.language-text white-space inherit .social text-align center
